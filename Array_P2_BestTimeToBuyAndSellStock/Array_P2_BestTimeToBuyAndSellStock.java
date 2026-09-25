a2795a1f9dac1522671cc2b74049a84fc4583ca0
Indhumathi <indhumathi.a29@gmail.com>
2026-09-25 15:53:17 +0530

    Array P2 - Best Time to Buy and Sell Stock: Add problem description Javadoc

--- Array_P2_BestTimeToBuyAndSellStock/Array_P2_BestTimeToBuyAndSellStock.java
+++ Array_P2_BestTimeToBuyAndSellStock/Array_P2_BestTimeToBuyAndSellStock.java
@@ -1,4 +1,16 @@
 package Array_P2_BestTimeToBuyAndSellStock;
 
+/**
+ * Array Problem 2: 121. Best Time to Buy and Sell Stock (LeetCode #121)
+ * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ *
+ * Description:
+ * You are given an array prices where prices[i] is the price of a given stock on the i-th day.
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ *
+ * Time Complexity: Brute Force O(N^2), DP Tabulation O(N), Optimal Greedy O(N)
+ * Space Complexity: Brute Force O(1), DP Tabulation O(N), Optimal Greedy O(1)
+ */
 public class Array_P2_BestTimeToBuyAndSellStock {
 }
