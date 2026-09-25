36f7a20d653edb90f8a06664141a2d1835fdc764
Indhumathi <indhumathi.a29@gmail.com>
2026-09-25 15:48:47 +0530

    Array P5 - Maximum Subarray: Implement Divide and Conquer approach (O(N log N))

--- Array_P5_MaximumSubarray/Array_P5_MaximumSubarray.java
+++ Array_P5_MaximumSubarray/Array_P5_MaximumSubarray.java
@@ -29,4 +29,38 @@ public class Array_P5_MaximumSubarray {
         }
         return maxSum;
     }
+
+    public int maxSubArrayDivideAndConquer(int[] nums) {
+        if (!validateInput(nums)) return 0;
+        return helperDivideAndConquer(nums, 0, nums.length - 1);
+    }
+
+    private int helperDivideAndConquer(int[] nums, int left, int right) {
+        if (left == right) return nums[left];
+
+        int mid = left + (right - left) / 2;
+        int leftMax = helperDivideAndConquer(nums, left, mid);
+        int rightMax = helperDivideAndConquer(nums, mid + 1, right);
+        int crossMax = maxCrossSubArray(nums, left, mid, right);
+
+        return Math.max(Math.max(leftMax, rightMax), crossMax);
+    }
+
+    private int maxCrossSubArray(int[] nums, int left, int mid, int right) {
+        int leftSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = mid; i >= left; i--) {
+            sum += nums[i];
+            leftSum = Math.max(leftSum, sum);
+        }
+
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            rightSum = Math.max(rightSum, sum);
+        }
+
+        return leftSum + rightSum;
+    }
 }
