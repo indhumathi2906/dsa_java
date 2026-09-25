e1a98b7df64fb1e82d4c231c1500cc1494601b6a
Indhumathi <indhumathi.a29@gmail.com>
2026-09-25 15:50:58 +0530

    Array P7 - Merge Intervals: Implement Sorting + ArrayList accumulator approach (O(N log N))

--- Array_P7_MergeIntervals/Array_P7_MergeIntervals.java
+++ Array_P7_MergeIntervals/Array_P7_MergeIntervals.java
@@ -42,4 +42,19 @@ public class Array_P7_MergeIntervals {
         }
         return result.toArray(new int[result.size()][]);
     }
+
+    public int[][] mergeSortingList(int[][] intervals) {
+        if (!validateIntervals(intervals)) return new int[0][0];
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
 }
