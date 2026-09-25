721b53a9583049c0baa9ccce9ed5572b35cdfbb3
Indhumathi <indhumathi.a29@gmail.com>
2026-09-25 15:48:42 +0530

    Array P5 - Maximum Subarray: Add problem description Javadoc

--- Array_P5_MaximumSubarray/Array_P5_MaximumSubarray.java
+++ Array_P5_MaximumSubarray/Array_P5_MaximumSubarray.java
@@ -1,4 +1,14 @@
 package Array_P5_MaximumSubarray;
 
+/**
+ * Array Problem 5: 53. Maximum Subarray (LeetCode #53)
+ * Link: https://leetcode.com/problems/maximum-subarray/
+ *
+ * Description:
+ * Given an integer array nums, find the subarray with the largest sum, and return its sum.
+ *
+ * Time Complexity: Brute Force O(N^2), Divide & Conquer O(N log N), Kadane's Algorithm O(N)
+ * Space Complexity: Brute Force O(1), Divide & Conquer O(log N) stack, Kadane's Algorithm O(1)
+ */
 public class Array_P5_MaximumSubarray {
 }
