ea9099115d066350ee66f221b3c7d834503d1509
Indhumathi <indhumathi.a29@gmail.com>
2026-09-25 15:50:56 +0530

    Array P7 - Merge Intervals: Implement Naive overlap scanner (O(N^2))

--- Array_P7_MergeIntervals/Array_P7_MergeIntervals.java
+++ Array_P7_MergeIntervals/Array_P7_MergeIntervals.java
@@ -1,5 +1,9 @@
 package Array_P7_MergeIntervals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Array Problem 7: 56. Merge Intervals (LeetCode #56)
  * Link: https://leetcode.com/problems/merge-intervals/
@@ -15,5 +19,27 @@ public class Array_P7_MergeIntervals {
 
     public boolean validateIntervals(int[][] intervals) {
         return intervals != null && intervals.length > 0;
+    }
+
+    public int[][] mergeBruteForce(int[][] intervals) {
+        if (!validateIntervals(intervals)) return new int[0][0];
+        int n = intervals.length;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        boolean[] merged = new boolean[n];
+        List<int[]> result = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (merged[i]) continue;
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            for (int j = i + 1; j < n; j++) {
+                if (!merged[j] && intervals[j][0] <= end) {
+                    end = Math.max(end, intervals[j][1]);
+                    merged[j] = true;
+                }
+            }
+            result.add(new int[]{start, end});
+        }
+        return result.toArray(new int[result.size()][]);
     }
 }
