c5dbb667bccaec290100a01ac4444fb3fb88fa8f
Indhumathi <indhumathi.a29@gmail.com>
2026-09-25 15:45:04 +0530

    Array P1 - Two Sum: Add problem description Javadoc

--- Array_P1_TwoSum/Array_P1_TwoSum.java
+++ Array_P1_TwoSum/Array_P1_TwoSum.java
@@ -1,4 +1,15 @@
 package Array_P1_TwoSum;
 
+/**
+ * Array Problem 1: 1. Two Sum (LeetCode #1)
+ * Link: https://leetcode.com/problems/two-sum/
+ *
+ * Description:
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Time Complexity: Brute Force O(N^2), Two Pointers O(N log N), Hash Map O(N)
+ * Space Complexity: Brute Force O(1), Two Pointers O(N), Hash Map O(N)
+ */
 public class Array_P1_TwoSum {
 }
