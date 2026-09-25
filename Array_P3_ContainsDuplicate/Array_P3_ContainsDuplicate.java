d4f14af31bce96858b71d9ea2ef26b7228f6c94e
Indhumathi <indhumathi.a29@gmail.com>
2026-09-25 15:53:34 +0530

    Array P3 - Contains Duplicate: Add problem description Javadoc

--- Array_P3_ContainsDuplicate/Array_P3_ContainsDuplicate.java
+++ Array_P3_ContainsDuplicate/Array_P3_ContainsDuplicate.java
@@ -1,4 +1,15 @@
 package Array_P3_ContainsDuplicate;
 
+/**
+ * Array Problem 3: 217. Contains Duplicate (LeetCode #217)
+ * Link: https://leetcode.com/problems/contains-duplicate/
+ *
+ * Description:
+ * Given an integer array nums, return true if any value appears at least twice in the array,
+ * and return false if every element is distinct.
+ *
+ * Time Complexity: Brute Force O(N^2), Sorting O(N log N), Hash Set O(N)
+ * Space Complexity: Brute Force O(1), Sorting O(1) auxiliary, Hash Set O(N)
+ */
 public class Array_P3_ContainsDuplicate {
 }
