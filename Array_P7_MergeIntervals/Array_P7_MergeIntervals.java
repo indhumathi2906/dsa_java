e6ada2fa31755d475ba72761c3db7d2a3dfc437e
Indhumathi <indhumathi.a29@gmail.com>
2026-09-25 15:50:53 +0530

    Array P7 - Merge Intervals: Add problem description Javadoc

--- Array_P7_MergeIntervals/Array_P7_MergeIntervals.java
+++ Array_P7_MergeIntervals/Array_P7_MergeIntervals.java
@@ -1,4 +1,15 @@
 package Array_P7_MergeIntervals;
 
+/**
+ * Array Problem 7: 56. Merge Intervals (LeetCode #56)
+ * Link: https://leetcode.com/problems/merge-intervals/
+ *
+ * Description:
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+ * and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ *
+ * Time Complexity: Brute Force O(N^2), Sorting + List O(N log N), Optimal Array Conversion O(N log N)
+ * Space Complexity: Brute Force O(N), Sorting + List O(N), Optimal Array Conversion O(N)
+ */
 public class Array_P7_MergeIntervals {
 }
